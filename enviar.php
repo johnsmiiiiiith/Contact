d8a62672e3a75583fbf23a92e98ad2c278b59a13
johnsmiiiiiith <111529540+johnsmiiiiiith@users.noreply.github.com>
2022-11-30 08:14:38 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -2,10 +2,10 @@
     $destino= "consultas@algogenial.com";
     $nombre= $_POST["Nombre"];
     $apellido= $_POST["Apellido"];
-    $teléfono= $_POST["Teléfono"];
+    $telefono= $_POST["Teléfono"];
     $mail= $_POST["Mail"];
     $mensaje= $_POST["Mensaje"];
-    $contenido= "Nombre:".$nNombre."\nApellido:".$Apellido."\nTeléfono:".$Teléfono."\nMail:".$Mail."\nMensaje:".$Mensaje;
+    $contenido= "Nombre:".$Nombre."\nApellido:".$Apellido."\nTeléfono:".$Telefono."\nMail:".$Mail."\nMensaje:".$Mensaje;
     mail($destino, "Contacto", $contenido);
     header("Location:Gracias.html");
 
